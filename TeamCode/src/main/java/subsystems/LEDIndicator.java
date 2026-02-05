4cb37aa56d307a01a7b834d09312e3704529aa5a
HyperhypeX <hyperhype22@gmail.com>
2026-02-05 16:11:12 -0600

    LED Indicator

---
+++ TeamCode/src/main/java/subsystems/LEDIndicator.java
@@ -0,0 +1,38 @@
+package subsystems;
+
+import static util.RobotConstants.Outtake.outtakeVelocityLong;
+import static util.RobotConstants.Outtake.outtakeVelocityShort;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LED;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import subsystems.Outtake;
+
+public class LEDIndicator {
+    private Servo ledServo;
+    private static final String LED_NAME = "robot_led";
+    private boolean isOn;
+    Outtake outtake = new Outtake();
+
+    public void init(HardwareMap hwMap) {
+        ledServo = hwMap.get(Servo.class, LED_NAME);
+        isOn = false;
+    }
+    public void setBrightness(double position) { //Brightness between 1.0 and 0.0
+        ledServo.setPosition(position);
+    }
+    public void turnOff() {
+        isOn = false;
+        setBrightness(0.0);
+    }
+    public void turnOn() {
+        setBrightness(1.0);
+        isOn = true;
+    }
+    public void setColorRed() {
+        setBrightness(0.700);
+    }
+}
